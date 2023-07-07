680610e14c05b85a8d8590f71d099fd76e48a415
Jan May <jan_may@gmx.net>
2023-07-07 23:10:54 +0200

    day8

---
+++ basics/reference_counting/src/main.rs
@@ -0,0 +1,14 @@
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a)); // Rc::clone does not make a deep copy
+    let c = Cons(4, Rc::clone(&a));
+}
